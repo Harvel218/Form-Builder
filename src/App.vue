1a5547b2fd887356fdb4a9941e74575e378a6714
unknown <chris@nextgm.ca>
2021-08-31 19:16:32 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,19 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  box-sizing: border-box;
+  min-width: 100%;
+  min-height: 100%;
 }
 
 #nav {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,68 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <QuestionCreator />
+    <label
+      ><span>Your question</span>
+      <input type="text" v-model="question" placeholder="How was your day?" />
+    </label>
+
+    <div class="question-type">
+      <span>Answer type</span>
+      <label
+        ><input
+          type="radio"
+          name="answer"
+          v-model="answer"
+          value="short"
+        /><span>short text</span></label
+      >
+      <label
+        ><input type="radio" name="answer" v-model="answer" value="long" /><span
+          >long text</span
+        ></label
+      >
+      <label
+        ><input
+          type="radio"
+          name="answer"
+          v-model="answer"
+          value="single"
+        /><span>one of multiple</span></label
+      >
+      <label
+        ><input
+          type="radio"
+          name="answer"
+          v-model="answer"
+          value="multiple"
+        /><span>multiple of multiple</span></label
+      >
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent, ref, watch } from "vue";
+import QuestionCreator from "../components/QuestionCreator.vue";
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
+  name: "Home",
+  components: { QuestionCreator },
+
+  setup() {
+    // const { question, shortText, longText, oneOfMultiple, multipleOfMultiple } =
+    //   ref<string>("");
+    const question = ref<string>("");
+    const answer = ref<string>("");
+
+    watch(
+      () => [question.value, answer.value],
+      (value) => {
+        console.log(value);
+      }
+    );
+
+    return { question, answer };
   },
 });
 </script>
